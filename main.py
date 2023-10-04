b3d93130ac61c2bf3d5777f6db02145b52d5d516
Ju5tu5 <108062300+Aqentjus@users.noreply.github.com>
2023-10-04 19:54:08 +0300

    .

--- main.py
+++ main.py
@@ -19,23 +19,23 @@ server_socket.listen(5)
 print(f"Server is listening on {host}:{port}")
 
 # JSON file path
-json_file_path = 'passwords.json'
+json_file_path = 'received_texts.json'
 
-# Function to save passwords to a JSON file
-def save_passwords(passwords):
+# Function to save received texts to a JSON file
+def save_to_json(received_texts):
     with open(json_file_path, 'w') as json_file:
-        json.dump(passwords, json_file)
+        json.dump(received_texts, json_file)
 
-# Function to load passwords from a JSON file
-def load_passwords():
+# Function to load received texts from a JSON file
+def load_from_json():
     if os.path.exists(json_file_path):
         try:
             with open(json_file_path, 'r') as json_file:
                 return json.load(json_file)
         except json.JSONDecodeError:
-            return {}
+            return []
     else:
-        return {}
+        return []
 
 while True:
     # Accept a client connection
@@ -46,6 +46,7 @@ while True:
         # Receive data from the client
         data = client_socket.recv(1024).decode('utf-8')
 
+        
         # Process the received data
         if data.startswith('/add '):
             data = data[len('/add '):]
@@ -56,7 +57,7 @@ while True:
                 continue
 
             # Load existing passwords
-            passwords = load_passwords()
+            passwords = load_from_json()
 
             # Add the new password to the existing data
             service = new_password.get('service')
@@ -64,12 +65,26 @@ while True:
             print(f"Received and stored password for service: {service}")
 
             # Save the updated data to the JSON file instantly
-            save_passwords(passwords)
+            save_to_json(passwords)
 
         elif data == '/get':
             # Load passwords from the JSON file when '/get' is requested
-            passwords = load_passwords()
+            passwords = load_from_json()
             client_socket.send(json.dumps(passwords).encode('utf-8'))
+
+        elif data.startswith('/get '):
+            # Extract the service name from the request
+            service_to_find = data[len('/get '):]
+
+            # Load passwords from the JSON file
+            passwords = load_from_json()
+
+            # Check if the requested service exists
+            if service_to_find in passwords:
+                response = json.dumps(passwords[service_to_find])
+                client_socket.send(response.encode('utf-8'))
+            else:
+                client_socket.send("Service not found.".encode('utf-8'))
 
         elif data == '/quit':
             break  # Exit the inner loop
