d06f165a718001800472f8de5268513242b8c2f7
Ju5tu5 <108062300+Aqentjus@users.noreply.github.com>
2023-10-04 16:48:03 +0300

    .

--- client.py
+++ client.py
@@ -1,4 +1,10 @@
 import socket
+import threading
+
+def receive_responses(client_socket):
+    while True:
+        response = client_socket.recv(1024).decode('utf-8')
+        print(response)
 
 # Create a socket object
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -10,6 +16,11 @@ server_port = 12345                      # Use the same port as in the server
 # Connect to the server
 client_socket.connect((server_host, server_port))
 
+# Start a thread for receiving and printing server responses
+response_thread = threading.Thread(target=receive_responses, args=(client_socket,))
+response_thread.daemon = True
+response_thread.start()
+
 while True:
     # Get user input
     user_input = input("Enter a command: ")
@@ -17,10 +28,9 @@ while True:
     # Send the user input to the server
     client_socket.send(user_input.encode('utf-8'))
 
-    # Receive and print the server's response
-    response = client_socket.recv(1024).decode('utf-8')
-    print(response)
+    # Check if the user wants to quit
+    if user_input == '/quit':
+        break
 
 # Close the client socket when done
-
 client_socket.close()
